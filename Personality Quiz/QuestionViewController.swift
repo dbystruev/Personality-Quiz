4adb94c916c5f08272371d7aa4416a1b6a194394
Denis Bystruev <dbystruev@me.com>
2018-09-10 20:54:32 +0300

    Added questions

--- Personality Quiz/QuestionViewController.swift
+++ Personality Quiz/QuestionViewController.swift
@@ -9,9 +9,45 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+    
+    var questions: [Question] = [
+        Question(text: "Какая еда вам нравится?",
+                 type: .single,
+                 answers: [
+                    Answer(text: "Стейк", type: .dog),
+                    Answer(text: "Рыба", type: .cat),
+                    Answer(text: "Морковка", type: .rabbit),
+                    Answer(text: "Кукуруза", type: .turtle)
+            ]),
+        
+        Question(text: "Что вам нравится делать?",
+                 type: .multiple,
+                 answers: [
+                    Answer(text: "Есть", type: .dog),
+                    Answer(text: "Спать", type: .cat),
+                    Answer(text: "Прыгать", type: .rabbit),
+                    Answer(text: "Плавать", type: .turtle)
+            ]),
+        
+        Question(text: "Насколько вам нравятся поездки на машине",
+                 type: .ranged,
+                 answers: [
+                    Answer(text: "Ненавижу их", type: .cat),
+                    Answer(text: "Немного нервничаю", type: .rabbit),
+                    Answer(text: "Почти не замечаю", type: .turtle),
+                    Answer(text: "Обожаю их", type: .dog),
+            ])
+    ]
+    
+    var questionIndex = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
+    }
+    
+    func updateUI() {
+        
     }
 
 }
