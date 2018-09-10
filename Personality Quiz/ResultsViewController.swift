7b40ee1b3d500c8ac294ad0d40e0486995ce779c
Denis Bystruev <dbystruev@me.com>
2018-09-10 23:17:10 +0300

    Finished the project

--- Personality Quiz/ResultsViewController.swift
+++ Personality Quiz/ResultsViewController.swift
@@ -17,6 +17,7 @@ class ResultsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.hidesBackButton = true
         calculatePersonalityResult()
     }
     
